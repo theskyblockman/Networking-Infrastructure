5dff4c425ac2386d8c313613fc78d5b3e73a1689
theskyblockman <theskyblockman@gmail.com>
2022-05-27 12:36:41 +0200

    Adding some classes

---
+++ src/main/java/fr/theskyblockman/networking_interface/server_type/ProxyConnexion.java
@@ -0,0 +1,15 @@
+package fr.theskyblockman.networking_interface.server_type;
+
+import java.net.Socket;
+
+/**
+ * @author theskyblockman
+ * @implNote The representation of a proxy to communicate with the proxy and not to a server through the proxy
+ */
+public class ProxyConnexion {
+    public String ip;
+    public int port;
+    public Socket socket;
+
+
+}
